f65cce76c64f1dc9267f41386285e3fc96b06c1e
Stuart Kol <skol@cs.washington.edu>
2019-10-02 16:02:15 -0700

    Things kind of work??

--- Project1/Receiver.py
+++ Project1/Receiver.py
@@ -1,5 +1,6 @@
 # Receiver Test
 import socket
+import struct
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDP_IP = "127.0.0.1"
@@ -9,20 +10,15 @@ print("Binding to: ", UDP_IP, " ", UDP_PORT)
 
 serversocket.bind((UDP_IP, UDP_PORT))
 
-# serversocket.listen(10)
-
 print("Server started and listening")
 
 while True:
-    # (clientsocket, address) = serversocket.accept()
-    # print("Connection found with: ", address)
-    # data = clientsocket.recv(1024).decode()
-    # print("Received message: ", data)
-    # message = "I hear you!"
-    # clientsocket.send(message.encode())
     data, addr = serversocket.recvfrom(1024)
-    i = 0
     print("Received Message:")
-    while i < len(data):
-        print(chr(data[i]))
+    ID = struct.unpack('!I', data[:4])[0]
+    print(ID)
+    PAYLOAD = data[4::]
+    i = 0
+    while i < len(PAYLOAD):
+        print(chr(PAYLOAD[i]))
         i += 1
--- Project1/Sender.py
+++ Project1/Sender.py
@@ -1,12 +1,12 @@
 # Send message test
 import socket
-import fixedint
+import struct
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = str(socket.htonl(fixedint.UInt32(100)))
+# MESSAGE = bytes([socket.htonl(fixedint.UInt32(100))])
+MESSAGE = struct.pack('!I', 461) + "Sup"
 
-print(100)
 print(MESSAGE)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
