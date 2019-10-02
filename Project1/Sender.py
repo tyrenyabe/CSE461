957ae34e4689991ee121842ba7af0c5683bcad6f
Stuart Kol <skol@cs.washington.edu>
2019-10-02 16:43:41 -0700

    I'm better than Yabe

--- Project1/Sender.py
+++ Project1/Sender.py
@@ -4,10 +4,8 @@ import struct
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-# MESSAGE = bytes([socket.htonl(fixedint.UInt32(100))])
-MESSAGE = struct.pack('!I', 461) + "Sup"
-
-print(MESSAGE)
+PAYLOAD = "sup"
+MESSAGE = struct.pack('!I', 461) + PAYLOAD.encode('utf-8')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
