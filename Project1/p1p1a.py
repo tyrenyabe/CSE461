8ef900e6cc40df59efdfabf7d6773c29120ce0fb
Stuart Kol <skol@cs.washington.edu>
2019-10-03 13:30:16 -0700

    Part 2 Complete

--- Project1/p1p1a.py
+++ Project1/p1p1a.py
@@ -14,5 +14,35 @@ sock.sendto(header, (UDP_IP, UDP_PORT))
 
 ans = sock.recv(1024)
 
-# (16, 0, 2, 363, 14, 50, 40047, 53)
 print(unpack('>IIHHIIII', ans))
+payload_len, psecret, step, sid, num, ln, UDP_PORT, secretA = unpack('>IIHHIIII', ans)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+i = 0
+while(i < num):
+    header = pack('>IIHHI', ln + 4, secretA, 1, 363, i)
+
+    if (ln % 4 != 0):
+        payload = bytearray(ln + (4 - (ln % 4)))
+    else:
+        payload = bytearray(ln)
+    message = header + payload
+
+    print(i, " | ", len(message))
+    sock.sendto(message, (UDP_IP, UDP_PORT))
+
+    try:
+        sock.settimeout(0.5)
+        ans = sock.recv(1024)
+        print(ans)
+    except socket.timeout:
+        i -= 1
+
+    i += 1
+
+ans = sock.recv(1024)
+print(unpack('>IIHHII', ans))
+
+payload_len, psecret, step, sid, TCP_PORT, secretB = unpack('>IIHHII', ans)
