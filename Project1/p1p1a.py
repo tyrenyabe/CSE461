ccbf9dcb4054ae7e5396b6735cc5c62031d90f3a
Tyren Dakujaku-Yabe <tyren.k.yabe@gmail.com>
2019-10-02 17:36:52 -0700

    don't need connect

--- Project1/p1p1a.py
+++ Project1/p1p1a.py
@@ -9,8 +9,6 @@ UDP_PORT = 12235
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
-sock.connect((UDP_IP, UDP_PORT))
-
 message = 'hello world' + '\0'
 header = pack('>IIHH', 12, 0, 1, 363) + message.encode('utf-8')
 
